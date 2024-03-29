4431fda62d8456e13ec32fad2d30d94306187b3c
user <user@Ubuntu.myguest.virtualbox.org>
2024-03-29 12:33:33 +0100

    Implemented the readwrite methods

--- readWrite.cpp
+++ readWrite.cpp
@@ -1,6 +1,23 @@
 #include <fstream>
 #include <iostream>
 
+void displayFileContents(std::ifstream& file) {
+    std::string line;
+    while (std::getline(file, line)) {
+        std::cout << line << std::endl;
+    }
+}
+
+void readAndDisplayFile(const std::string& filePath) {
+    std::ifstream file(filePath);
+    if (!file) {
+        std::cerr << "Unable to open file: " << filePath << std::endl;
+        return;
+    }
+
+    displayFileContents(file);
+}
+
 int main() {
     readAndDisplayFile("vhod.txt");
     readAndDisplayFile("izhod.txt");
