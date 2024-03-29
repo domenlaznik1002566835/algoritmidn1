10c1e36ea22a140861b972605c5477f15c9d9641
user <user@Ubuntu.myguest.virtualbox.org>
2024-03-29 11:49:18 +0100

    Implemented input file reading

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <iterator>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::ifstream file("vhod.txt");
+    std::vector<int> numbers;
+
+    if (!file) {
+        std::cerr << "Unable to open file";
+        return 1;
+    }
+
+    std::copy(std::istream_iterator<int>(file),
+              std::istream_iterator<int>(),
+              std::back_inserter(numbers));
+
     return 0;
 }
